36e62211a6e13870376176e0224a14c695c15f41
k0sm0s21 <145329561+k0sm0s21@users.noreply.github.com>
2023-10-23 11:33:18 +0300

    Add files via upload

---
+++ 6/6/6.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool is_power_of_two(int n) {
+    return n > 0 && (n & (n - 1)) == 0;
+}
+
+int main() {
+    setlocale(0, "");
+    int n;
+    cout << "Введите число: ";
+    cin >> n;
+
+    if (is_power_of_two(n)) {
+        cout << "Число является степенью двойки" << endl;
+    }
+    else {
+        cout << "Число не является степенью двойки" << endl;
+    }
+
+    return 0;
+}
---
+++ 9/9/9.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool is_perfect_number(int n) {
+    int sum = 1;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            sum += i;
+            if (n / i != i) {
+                sum += n / i;
+            }
+        }
+    }
+    return sum == n;
+}
+
+int main() {
+    setlocale(0, "");
+    int n;
+    cout << "Введите число: ";
+    cin >> n;
+
+    if (is_perfect_number(n)) {
+        cout << "Число является совершенным" << endl;
+    }
+    else {
+        cout << "Число не является совершенным" << endl;
+    }
+
+    return 0;
+}
