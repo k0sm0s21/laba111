f2ea86c5a592da0cb9c6d25944687b15fdd15a45
k0sm0s21 <145329561+k0sm0s21@users.noreply.github.com>
2023-10-23 11:18:35 +0300

    Add files via upload

---
+++ 5/5/5.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool is_divisible_by_3_and_5(int n) {
+    return n % 3 == 0 && n % 5 == 0;
+}
+
+int main() {
+    setlocale(0, "");
+    int n;
+    cout << "Введите число: ";
+    cin >> n;
+
+    if (is_divisible_by_3_and_5(n)) {
+        cout << "Число кратно 3 и 5" << endl;
+    }
+    else {
+        cout << "Число не кратно 3 и 5" << endl;
+    }
+
+    return 0;
+}
