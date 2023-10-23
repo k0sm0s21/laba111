f72098a67184de3239b0e91a10fdd78bd78ba4e2
k0sm0s21 <145329561+k0sm0s21@users.noreply.github.com>
2023-10-23 11:02:57 +0300

    Add files via upload

---
+++ 2/2/2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    int n;
+    cout << "Введите число: ";
+    cin >> n;
+
+    if (n > 0) {
+        cout << "Число положительное" << endl;
+    }
+    else if (n < 0) {
+        cout << "Число отрицательное" << endl;
+    }
+    else {
+        cout << "Число равно нулю" << endl;
+    }
+
+    return 0;
+}
