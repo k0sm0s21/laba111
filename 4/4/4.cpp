0763810e920c416feab669c05096a6e419966095
k0sm0s21 <145329561+k0sm0s21@users.noreply.github.com>
2023-10-23 11:13:32 +0300

    Add files via upload

---
+++ 4/4/4.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool is_palindrome(string str) {
+    int n = str.length();
+
+    for (int i = 0; i < n / 2; i++) {
+        if (str[i] != str[n - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    setlocale(0, "");
+    string str;
+    cout << "Введите строку: ";
+    cin >> str;
+
+    if (is_palindrome(str)) {
+        cout << "Строка является палиндромом" << endl;
+    }
+    else {
+        cout << "Строка не является палиндромом" << endl;
+    }
+
+    return 0;
+}
