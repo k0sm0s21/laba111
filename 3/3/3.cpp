23e8c171fc2be78025b4fe0fa0d639ef0c7595b2
k0sm0s21 <145329561+k0sm0s21@users.noreply.github.com>
2023-10-23 11:04:23 +0300

    Add files via upload

---
+++ 3/3/3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+bool is_leap_year(int year) {
+    return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+}
+
+int main() {
+    setlocale(0, "");
+    int year;
+    cout << "Введите год: ";
+    cin >> year;
+
+    if (is_leap_year(year)) {
+        cout << "Год високосный" << endl;
+    }
+    else {
+        cout << "Год не високосный" << endl;
+    }
+
+    return 0;
+}
