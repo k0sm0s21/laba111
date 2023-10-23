dd3190037f802baa2231ae5d7893cfa3a364bdea
k0sm0s21 <145329561+k0sm0s21@users.noreply.github.com>
2023-10-23 13:12:32 +0300

    Add files via upload

---
+++ 7/7/7.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+	bool is_centennial(int year) {
+		return year % 100 == 0;
+ 	}
+	int main() {
+		setlocale(0, "");
+		int year;
+			cout << "Введите год: ";
+		cin >> year;
+
+		if (is_centennial(year)) {
+			cout << "Год является вековым" << endl;
+		}
+		else {
+			cout << "Год не является вековым" << endl;
+		}
+		return 0;
+	}
