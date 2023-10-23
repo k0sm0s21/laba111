60b0c9f6a9eb3fd7d9c7b784b9c6eb16c231991b
gleb <natniv05@rambler.ru>
2023-10-23 10:55:27 +0300

    Добавьте файлы проекта.

---
+++ zadanie1/zadanie1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool is_prime(int n) {
+    setlocale(0, "");
+    if (n <= 1) {
+        return false;
+    }
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+int main() {
+    setlocale(0, "");
+    cout << "Введите число: ";
+    int n;
+    cin >> n;
+
+    if (is_prime(n)) {
+        cout << "Число простое" << endl;
+    }
+    else {
+        cout << "Число составное" << endl;
+    }
+
+    return 0;
+}
